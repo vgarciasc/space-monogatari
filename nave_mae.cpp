0035eca19b5ae06439f17f06cb342168848e6f6e
diegosouza313 <diegosouza313@gmail.com>
2015-06-26 19:40:27 -0300

    Update nave_mae.cpp

--- nave_mae.cpp
+++ nave_mae.cpp
@@ -1,39 +1,66 @@
-#ifndef NAVE_MAE_H
-#define NAVE_MAE_H
+#include <stdio.h>
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_image.h>
+#include "game.h"
+#include "nave_mae.h"
 
-struct Jogo;
+void desenha_nave(NaveMae *nave,Jogo *jogo, Projetil *projetil){
+	if(autoriza_nave(nave,jogo) || nave->status==1){
+	 	 al_draw_bitmap(nave->imagem, nave->posicao_x,nave->posicao_y,0);
+	 	 if(nave->posicao_x>=jogo->largura){
+	 		 reinicia_nave(nave);
+	 		 return;
+	 	 }
+	 	 detecta_colisao(nave, jogo);
+	 	 movimenta_nave(nave);
 
-struct NaveMae{
-	ALLEGRO_BITMAP *imagem=NULL;
-	int posicao_x;
-	int posicao_y;
-	int velocidade;
-	int status;
-	int altura;
-	int largura;
-};
+	}
 
-//desenha a nave de acordo com as coordenadas
-//apenas essa aqui será usada no loop do jogo
-void desenha_nave(NaveMae*, Jogo*, Projetil*);
+}
+void movimenta_nave(NaveMae *nave){
+	nave->posicao_x +=nave->velocidade;
+}
+void reinicia_nave(NaveMae *nave){
+	nave->posicao_x = -35;
+	nave->posicao_y = 0;
+	nave->status = 0;
+}
+void inicializa_nave(NaveMae *nave){
+	nave->imagem = al_load_bitmap("resources/nave.png");
+	if (nave->imagem == NULL) {
+			puts("Erro ao carregar o arquivo resources/nave.png");
+			exit(0);
+	}
+	nave->velocidade=3;
+	nave->largura=35;
+	nave->altura=35;
+	reinicia_nave(nave);
+}
+void finaliza_nave(NaveMae *nave){
+	al_destroy_bitmap(nave->imagem);
+}
+void detecta_colisao(NaveMae *nave, Jogo *jogo){
+	for (int i = 0; i < jogo->numero_de_projeteis; i++) {
 
-// por enquanto apenas coloca posicao_x=0 e posicao_y=0[depois faz mais]
-// AINDA PRECISA REINICIAR O CONTADOR AQUI
-void reinicia_nave(NaveMae*);
+		if(!(jogo->projetil_stack[i].posicao_x > nave->posicao_x + nave->largura ||
+				jogo->projetil_stack[i].posicao_y > nave->posicao_y + nave->altura  ||
+				jogo->projetil_stack[i].posicao_y + jogo->projetil_stack[i].altura_sprite< nave->posicao_y ||
+				jogo->projetil_stack[i].posicao_x + jogo->projetil_stack[i].largura_sprite < nave->posicao_x))
+		{
 
-// responsável pela mudança nas coordenadas
-void movimenta_nave(NaveMae*);
+				reinicia_nave(nave);
 
-// carrega a imagem em nave->imagem, reinicia as posições e define a velocidade
-//utilizar antes da inicialização do loop do jogo
-void inicializa_nave(NaveMae*);
+				return ;
+		}
 
-//função que controla se a nave mae aparece ou n
-int autoriza_nave(NaveMae*, Jogo*);
+	}
 
-void detecta_colisao(NaveMae*, Jogo*);
-
-//destroi a nave mae
-void finaliza_nave(NaveMae*);
-
-#endif
+}
+int autoriza_nave(NaveMae *nave, Jogo *jogo){
+	if(((jogo->segundos)/FPS)%10 == 0){
+		nave->status=1;
+		return 1;
+	}
+	else
+		return 0;
+}
