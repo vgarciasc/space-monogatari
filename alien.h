fa60885f1b893f6d3cae4adf9e0e323b0e541c5e
juliorkm <julioramakrsnamandoju@gmail.com>
2015-06-27 11:33:56 -0300

    Update alien.h

--- alien.h
+++ alien.h
@@ -8,16 +8,20 @@
 
 #define LINHAS_TROPA 5
 #define COLUNAS_TROPA 10
-#define DISTANCIA_PASSO_ALIEN 1
+#define DISTANCIA_PASSO_ALIEN 20
+
+#define altura_aliens 35
+#define largura_aliens 36
+
+struct Jogo;
 
 struct Alien {
 	int posicao_x, posicao_y;
-	int delta_x;
+	int delta_x, delta_y;
 	DIRECAO direcao_atual;
 
 	bool vivo;
 
-	int frame_count;
 	int sprite_atual;
 	ALLEGRO_BITMAP* sprites[10];
 };
@@ -36,11 +40,13 @@ void inicializa_sprites_alien (Alien* alien);
 
 void finaliza_sprites_alien (Alien* alien);
 
+void muda_direcao_tropa (Alien alien[COLUNAS_TROPA][LINHAS_TROPA], DIRECAO direcao);
+
 void move_alien (Alien* alien, DIRECAO direcao);
 
 void move_tropa (Alien alien[COLUNAS_TROPA][LINHAS_TROPA], DIRECAO direcao);
 
-void rota_tropa (Alien alien[COLUNAS_TROPA][LINHAS_TROPA]);
+void rota_tropa (Alien alien[COLUNAS_TROPA][LINHAS_TROPA], Jogo* jogo);
 
 int get_posicao_x_max_alien (Alien* alien);
 
