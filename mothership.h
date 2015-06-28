2983bd8792e676d17d0d9beb93bc3e7566d7d3db
diegosouza313 <diegosouza313@gmail.com>
2015-06-27 22:34:54 -0300

    Update mothership.h

--- mothership.h
+++ mothership.h
@@ -6,18 +6,22 @@ struct Jogo;
 struct Mothership{
 	ALLEGRO_BITMAP *imagem=NULL;
 	ALLEGRO_FONT *fonte=NULL;
+	ALLEGRO_TIMER *tempo=NULL;
+	ALLEGRO_EVENT_QUEUE *fila_evento = NULL;
+	int segundos;
 	int posicao_x;
 	int posicao_y;
 	int velocidade;
 	int frequencia;
 	int status;
-	int altura;
-	int largura;
+	int altura_sprite;
+	int largura_sprite;
+	int largura_tela;
 };
 
 //desenha a mothership de acordo com as coordenadas
 //apenas essa aqui será usada no loop do jogo
-void desenha_mothership(Mothership*, Jogo*, Projetil*);
+void desenha_mothership(Mothership*);
 
 // por enquanto apenas coloca posicao_x=0 e posicao_y=0[depois faz mais]
 // AINDA PRECISA REINICIAR O CONTADOR AQUI
@@ -28,14 +32,18 @@ void movimenta_mothership(Mothership*);
 
 // carrega a imagem em mothership->imagem, reinicia as posições e define a velocidade
 //utilizar antes da inicialização do loop do jogo
-void inicializa_mothership(Mothership*);
+void inicializa_mothership(Mothership*, Jogo*);
 
 //função que controla se a mothership mae aparece ou n
-int autoriza_mothership(Mothership*, Jogo*);
+int autoriza_mothership(Mothership*);
 
-void detecta_colisao(Mothership*, Jogo*);
+void inicializar_timer_mothership(Mothership*);
+
+void disparar_timer_mothership(Mothership*);
+
 
 //destroi a mothership mae
 void finaliza_mothership(Mothership*);
 
 #endif
+
