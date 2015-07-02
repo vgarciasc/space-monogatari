034d2a19103c52fd0a0471d7a9ecfbcbc3ea5989
diegosouza313 <diegosouza313@gmail.com>
2015-07-02 19:55:18 -0300

    Update hud.h

--- hud.h
+++ hud.h
@@ -4,14 +4,21 @@
 #include "config.h"
 
 struct Hud {
+	ALLEGRO_FONT *font=NULL;
+	ALLEGRO_BITMAP *life=NULL;
+	ALLEGRO_EVENT_QUEUE *fila_eventos = NULL;
+	FILE *arquivo_high_score;
 	int score, score_posicao_x;
 	int lives, lives_posicao_x;
+
 	double altura_bitmap_vida, largura_bitmap_vida;
 
-	ALLEGRO_FONT *font;
-	ALLEGRO_BITMAP *life;
-
 	int posicao_y;
+
+	char nome_score[3];
+
+	char nomes_high_score[10][3];
+	int scores_high_score[10];
 };
 
 void inicializa_hud (Hud *hud);
@@ -26,4 +33,12 @@ void desenha_score (Hud* hud);
 
 void desenha_lives (Hud* hud);
 
+void ler_high_score(Hud *hud);
+void iniciar_salvar_score(Hud *hud);
+void inserir_caracter(Hud* hud, char letra);
+void apagar_caracter(Hud* hud);
+void permutar_strings(char *nome1, char *nome2);
+void inicializar_high_score(Hud *hud);
+void enviar_score(Hud *hud);
+void ordenar_vetor(int *vetor, char nomes[][3], int n_elementos);
 #endif
